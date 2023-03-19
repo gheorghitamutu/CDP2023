710ea61ad987dface3dbeef49a88fe3391dd10c1
Gheorghiță Mutu <gheorghitamutu@gmail.com>
2023-03-20 00:52:31 +0200

    [Homework02] # fixed API project

--- Homework02/StockTickerAPI/StockTickerAPI/Controllers/Stocks.cs
+++ Homework02/StockTickerAPI/StockTickerAPI/Controllers/Stocks.cs
@@ -1,27 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Linq.Expressions;
-using Azure.Messaging.EventHubs;
-using Azure.Messaging.EventHubs.Consumer;
-using Azure.Messaging.EventHubs.Processor;
-using Azure.Storage.Blobs;
-using System.Text;
 using Microsoft.Azure.Cosmos;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs;
-using Newtonsoft.Json.Linq;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
+using Azure.Messaging.EventHubs;
 
 namespace StockTickerAPI.Controllers
 {
     [ApiController]
     [Route("/get_stocks")]
-    public class Stocks : ControllerBase
+    public class GetStocksController : ControllerBase
     {
         // The Azure Cosmos DB endpoint for running this sample.
-        private static readonly string? EndpointUri = System.Configuration.ConfigurationManager.AppSettings["EndPointUri"];
+        private static readonly string? EndpointUri = "https://stockticker.documents.azure.com:443/";
 
         // The primary key for the Azure Cosmos account.
-        private static readonly string? PrimaryKey = System.Configuration.ConfigurationManager.AppSettings["PrimaryKey"];
+        private static readonly string? PrimaryKey = "wQD9JhuEn1O7rcE2MoB9sgairaMZ25VvYCMIB8t4KN6dshyqF815sN3lIvVgKt4O5vlfhHZsI3BHACDbmWXMqA==";
 
         // The Cosmos client instance
         private CosmosClient? cosmosClient;
@@ -43,14 +36,37 @@ namespace StockTickerAPI.Controllers
         // private readonly static EventProcessorClient processor = new(
         //     storageClient, EventHubConsumerClient.DefaultConsumerGroupName, EVENT_HUB_CONNECTION_STRING, "stockticker_1679176237014");
 
-        private readonly ILogger<Stocks> _logger;
+        private readonly ILogger<GetStocksController> _logger;
 
-        public Stocks(ILogger<Stocks> logger) => _logger = logger;
+        public GetStocksController(ILogger<GetStocksController> logger) => _logger = logger;
 
         [HttpGet(Name = "GetStockTicker")]
         public async Task<IEnumerable<StockTicker>> Get()
         {
-            await Initialize();
+            {
+                // Create a new instance of the Cosmos Client
+                cosmosClient = new CosmosClient(EndpointUri, PrimaryKey, new CosmosClientOptions() { ApplicationName = "StockTickerAPI" });
+                database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+                container = await database.CreateContainerIfNotExistsAsync(containerId, "/partitionKey");
+
+                try
+                {
+                    int? throughput = await container.ReadThroughputAsync();
+                    if (throughput.HasValue)
+                    {
+                        _logger.LogInformation($"Current provisioned throughput : {throughput.Value}");
+                        int newThroughput = throughput.Value + 100;
+                        // Update throughput
+                        await container.ReplaceThroughputAsync(newThroughput);
+                        _logger.LogInformation($"New provisioned throughput : : {newThroughput}");
+                    }
+                }
+                catch (CosmosException cosmosException) when (cosmosException.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    _logger.LogError("Cannot read container throuthput.");
+                    _logger.LogError($"Exception : {cosmosException.ResponseBody}");
+                }
+            }
 
             var sqlQueryText = "SELECT * FROM c";
             Console.WriteLine("Running query: {0}\n", sqlQueryText);
@@ -68,76 +84,46 @@ namespace StockTickerAPI.Controllers
                 }
             }
 
+            cosmosClient.Dispose();
+
             return stocks;
-        }
-
-        public async Task Initialize()
-        {
-            // Create a new instance of the Cosmos Client
-            cosmosClient = new CosmosClient(EndpointUri, PrimaryKey, new CosmosClientOptions() { ApplicationName = "StockTickerAPI" });
-            database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
-            container = await database.CreateContainerIfNotExistsAsync(containerId, "/partitionKey");
-
-            try
-            {
-                int? throughput = await container.ReadThroughputAsync();
-                if (throughput.HasValue)
-                {
-                    _logger.LogInformation($"Current provisioned throughput : {throughput.Value}");
-                    int newThroughput = throughput.Value + 100;
-                    // Update throughput
-                    await container.ReplaceThroughputAsync(newThroughput);
-                    _logger.LogInformation($"New provisioned throughput : : {newThroughput}");
-                }
-            }
-            catch (CosmosException cosmosException) when (cosmosException.StatusCode == System.Net.HttpStatusCode.BadRequest)
-            {
-                _logger.LogError("Cannot read container throuthput.");
-                _logger.LogError($"Exception : {cosmosException.ResponseBody}");
-            }
         }
     }
 
-
     [ApiController]
     [Route("/set_stock")]
-    public class SetStock : ControllerBase
+    public class SetStockController : ControllerBase
     {
         private readonly static string? EVENT_HUB_CONNECTION_STRING = Environment.GetEnvironmentVariable("EventHub_stockticker_1679176237014");
-        // private readonly static string? BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("BlobStorage_stockticker");
+        private readonly static string? BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("BlobStorage_stockticker");
         const string EVENT_HUB_CONNECTION_STRING_PLAIN = "Endpoint=sb://stock-ticker.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q6Xjs7/o1044U2SH/RwKNB4UbcWuzPfe6+AEhAdW6uQ=";
 
-        [FunctionName("SetStock")]
-        public static async Task<Microsoft.AspNetCore.Mvc.IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "/sendStockData")] HttpRequest req,
-        [ServiceBus("StockTickerData", Connection = EVENT_HUB_CONNECTION_STRING_PLAIN)] IAsyncCollector<string> eventHubMessages,
-        ILogger log)
+        private readonly ILogger<SetStockController> _logger;
+
+        public SetStockController(ILogger<SetStockController> logger) => _logger = logger;
+
+        [HttpPost(Name = "SetStock")]
+        public async Task<IActionResult> SetStock(string symbol, double price, int volume, double change, double changePercent, DateTime date)
         {
-            log.LogInformation($"StockTickerFunction HTTP trigger function started {req}.");
+            _logger.LogInformation($"StockTickerFunction HTTP trigger function started {HttpContext.Response}.");
 
-            if (req.ContentType == "application/json")
+            StockTicker st = new()
             {
-                // Send stock data to Event Hub
+                Symbol = symbol,
+                Price = price,
+                Volume = volume,
+                Change = change,
+                ChangePercent = changePercent,
+                Date = date
+            };
 
-                var data = JObject.Parse(req.Body.ToString());
-                StockTicker st = new()
-                {
-                    Symbol = data["Symbol"].ToString(),
-                    Price = double.Parse(data["Price"].ToString()),
-                    Volume = int.Parse(data["Volume"].ToString()),
-                    Change = double.Parse(data["Change"].ToString()),
-                    ChangePercent = double.Parse(data["ChangePercent"].ToString()),
-                    Date = DateTime.Parse(data["Date"].ToString())
-                };
+            EventHubProducerClient client = new(EVENT_HUB_CONNECTION_STRING_PLAIN, "StockTickerData");
+            using EventDataBatch batch = await client.CreateBatchAsync();
+            batch.TryAdd(new EventData(Encoding.UTF8.GetBytes(st.ToString())));
+            await client.SendAsync(batch);
+            _logger.LogInformation($"Stock data sent to Event Hub: {st}");
 
-
-                await eventHubMessages.AddAsync(st.ToString());
-                log.LogInformation($"Stock data sent to Event Hub: {st}");
-
-                return new OkResult();
-            }
-
-            return new BadRequestResult();
+            return new OkResult();
         }
     }
 }
--- Homework02/StockTickerAPI/StockTickerAPI/Program.cs
+++ Homework02/StockTickerAPI/StockTickerAPI/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+    c.IgnoreObsoleteActions();
+    c.IgnoreObsoleteProperties();
+    c.CustomSchemaIds(type => type.FullName);
+});
 
 builder.Services.ConfigureSwaggerGen(setup =>
 {

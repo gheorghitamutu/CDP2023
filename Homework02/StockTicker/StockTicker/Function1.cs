4f48372ab8801e05663f0c4254127d8cded48d59
Gheorghiță Mutu <gheorghitamutu@gmail.com>
2023-03-19 20:30:01 +0200

    [Homework02] + modified function app sending data to event hub

--- Homework02/StockTicker/StockTicker/Function1.cs
+++ Homework02/StockTicker/StockTicker/Function1.cs
@@ -7,16 +7,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using System.Net.WebSockets;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Text;
 
 public static class StockTickerFunction
 {
-    [FunctionName("StockTickerFunction")]
+    // used in attributes:
+    const string EVENT_HUB_CONNECTION_STRING_PLAIN = "Endpoint=sb://stock-ticker.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q6Xjs7/o1044U2SH/RwKNB4UbcWuzPfe6+AEhAdW6uQ=";
+
+    // used.. somewhere else?
+    public readonly static Expression EVENT_HUB_CONNECTION_STRING = Expression.Constant(Environment.GetEnvironmentVariable("EventHub_stockticker_1679176237014"));
+    public readonly static Expression BLOB_STORAGE_CONNECTION_STRING = Expression.Constant(Environment.GetEnvironmentVariable("BlobStorage_stockticker"));
+
+    [FunctionName("SendStockDataToEventHubViaWebSockets")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "StockTicker")] HttpRequest req,
-        [ServiceBus("StockTickerRealTime", Connection = "Endpoint=sb://stock-ticker.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Q6Xjs7/o1044U2SH/RwKNB4UbcWuzPfe6+AEhAdW6uQ=")] IAsyncCollector<string> eventHubMessages,
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "/sendStockData")] HttpRequest req,
+        [ServiceBus("StockTickerRealTime", Connection = EVENT_HUB_CONNECTION_STRING_PLAIN)] IAsyncCollector<string> eventHubMessages,
         ILogger log)
     {
-        log.LogInformation("StockTickerFunction HTTP trigger function started.");
+        log.LogInformation($"StockTickerFunction HTTP trigger function started {req}.");
 
         // Get the WebSocket connection
         WebSocket webSocket = await req.HttpContext.WebSockets.AcceptWebSocketAsync();
@@ -30,17 +40,80 @@ public static class StockTickerFunction
 
             if (result.MessageType == WebSocketMessageType.Text)
             {
-                var stockData = JObject.Parse(System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count));
-                string stockSymbol = stockData["symbol"].ToString();
-                double stockValue = double.Parse(stockData["value"].ToString());
+                var data = JObject.Parse(Encoding.UTF8.GetString(buffer, 0, result.Count));
+
+                string symbol = data["Symbol"].ToString();
+                double price = double.Parse(data["Price"].ToString());
+                int volume = int.Parse(data["Volume"].ToString());
+                double change = double.Parse(data["Change"].ToString());
+                double changePercent = double.Parse(data["ChangePercent"].ToString());
+                DateTime date = DateTime.Parse(data["Date"].ToString());
 
                 // Send stock data to Event Hub
-                await eventHubMessages.AddAsync($"{stockSymbol},{stockValue}");
-                log.LogInformation($"Stock data sent to Event Hub: {stockSymbol},{stockValue}");
+                var dataString = $"{symbol},{price},{volume},{change},{changePercent},{date}";
+                await eventHubMessages.AddAsync(dataString);
+                log.LogInformation($"Stock data sent to Event Hub: {dataString}");
             }
         }
 
         return new OkResult();
     }
-}
 
+
+    [FunctionName("GenerateStockDataToEventHub")]
+    public static async Task<IActionResult> Run([TimerTrigger("0 */30 * * * *")] TimerInfo _,
+        [ServiceBus("StockTickerRealTime", Connection = EVENT_HUB_CONNECTION_STRING_PLAIN)] IAsyncCollector<string> eventHubMessages,
+        ILogger log)
+    {
+        Console.WriteLine($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        List<StockTicker> stockData = GenerateStockData();
+
+        foreach(var data in stockData)
+        {
+            var dataString = $"{data.Symbol},{data.Price},{data.Volume},{data.Change},{data.ChangePercent},{data.Date}";
+            await eventHubMessages.AddAsync(dataString);
+            log.LogInformation($"Stock data sent to Event Hub: {dataString}");
+        }
+
+        return new OkResult();
+    }
+
+    private static List<StockTicker> GenerateStockData()
+    {
+        Random rnd = new();
+        List<StockTicker> stockData = new();
+
+        List<string> symbols = new() { "AAPL", "AMZN", "GOOGL", "TSLA", "MSFT", "NFLX", "NVDA", "PYPL", "FB", "JPM" };
+
+        foreach (var symbol in symbols)
+        {
+            stockData.Add(new StockTicker()
+            {
+                Symbol = symbol,
+                Price = rnd.Next(100, 1000),
+                Volume = rnd.Next(100000, 1000000),
+                Change = rnd.Next(-10, 10),
+                ChangePercent = rnd.Next(-1000, 1000) / 100.0,
+                Date = DateTime.UtcNow
+            });
+        }
+
+        return stockData;
+    }
+
+    public class StockTicker
+    {
+        public string? Symbol { get; set; }
+
+        public double Price { get; set; }
+
+        public int Volume { get; set; }
+
+        public double Change { get; set; }
+
+        public double ChangePercent { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
